6d7adb32188792b3f19d87611afde8acb459f5ad
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-08-02 16:48:39 -0700

    joining two tables and taking care of missing values

--- TitanicKaggle.R
+++ TitanicKaggle.R
@@ -2,3 +2,17 @@ setwd("~/GitHub/Titanic-Kaggle")
 
 titanic.train <- read.csv(file = "train.csv", stringsAsFactors = FALSE, header = TRUE)
 titanic.test <- read.csv(file = "test.csv",stringsAsFactors = FALSE, header = TRUE)
+
+#combining both datasets
+#first adding a new feature to differentiate between both dataset before combining
+titanic.train$IsTrainSet <- TRUE
+titanic.test$IsTrainSet <- FALSE
+
+# adding survived column to the test set
+titanic.test$Survived<- NA
+
+#Joining the two datasets
+titanic.full <- rbind(titanic.train,titanic.test)
+
+#filling the missing string in embarked with mode of the titanic.full
+titanic.full[titanic.full$Embarked == '',"Embarked"] <- 'S'
